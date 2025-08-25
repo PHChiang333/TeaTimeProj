93fec1d7481cbe8627d049e1fab8d89b07823a76
PHChiang333 <frank79530@gmail.com>
2025-08-25 12:44:22 +0800

    feat: replace bootstrap.cs with bootswatch_flatly

---
+++ TeaTimeProj/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeaTimeProj.Models;
+
+namespace TeaTimeProj.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //預設資料
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "茶飲", DisplayOrrder = 1 },
+                new Category { Id = 2, Name = "水果茶", DisplayOrrder = 2 },
+                new Category { Id = 3, Name = "咖啡", DisplayOrrder = 3 }
+            );
+        }
+    }
+}
